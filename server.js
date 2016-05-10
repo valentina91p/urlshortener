182063a6d7b49abcba545cfce920a6b8d4d466fd
c-valentinamm <c.valentinamm@gmail.com>
2016-05-10 13:34:46 -0400

    url is now being validated

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var app = require('express')();
 
 app.get('/',function(req,res){});
-app.get('/create/:url',function(req,res){});
+app.get("/create/*", function(req,res){
+	console.log(req.params[0]);
+	if(/^(http|https):\/\/www\.[a-zA-Z][a-zA-Z\-]*\.[a-zA-Z]+$/.test(req.params[0]))
+		res.json("Valid url.");
+	else
+		res.json({error:"Wrong url format, make sure you have a valid protocol and real site."});
+})
 app.get('/:url',function(req,res){});
 
 app.listen(process.env.PORT || 80, function(){
