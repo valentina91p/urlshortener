712c698583eddab39a5c9eba0f0780bf011ffdb4
c-valentinamm <c.valentinamm@gmail.com>
2016-05-10 18:56:44 -0400

    added connection to db with mongoose when an url is submitted to /create a new token is generated and save with the original url, new short url is returned when a short url is submitted it redirects the user to the original url

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 var app = require('express')();
+var mongoose = require('mongoose');
+var tokengen = require('rand-token');
 
-app.get('/',function(req,res){});
+mongoose.connect('mongodb://127.0.0.1/urlshortener');
+
+var urlSchema = mongoose.Schema({
+	token: String,
+	original: String
+});
+
+var Url = mongoose.model('Url', urlSchema);
+
+app.get('/',function(req,res){
+
+});
+
 app.get("/create/*", function(req,res){
-	console.log(req.params[0]);
-	if(/^(http|https):\/\/www\.[a-zA-Z][a-zA-Z\-]*\.[a-zA-Z]+$/.test(req.params[0]))
-		res.json("Valid url.");
-	else
+	if(/^(http|https):\/\/www\.[a-zA-Z][a-zA-Z\-]*\.[a-zA-Z]+$/.test(req.params[0])){
+		var new_token = tokengen.generate(4);
+		Url.save({token: new_token, original: req.params[0]}, function(err,url){
+			if(err){
+				res.json({error: err});	
+			}else{
+				res.json({original_url: url.original, short_url: url.token});
+			}
+		});
+	}else
 		res.json({error:"Wrong url format, make sure you have a valid protocol and real site."});
 })
-app.get('/:url',function(req,res){});
+app.get('/:url',function(req,res){
+	Url.find({token: req.params.url}, function(err, url){
+		if(err){
+			res.json({error: err});
+		}else{
+			res.redirect(url[0].original);
+		}
+	});
+});
 
 app.listen(process.env.PORT || 80, function(){
 
